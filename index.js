e6af53d5fc47a6d1b407d6434e6bb32a7c757f7a
github username <github email address>
2020-11-10 14:55:03 -0500

    In Progress

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const selectorThree = document.querySelector(".selector3")
 const selectorFour = document.querySelector(".selector4")
 const selectorFive = document.querySelector(".selector5")
 const kickContainer = document.querySelector(".kick-container")
-const addSneakerForm = document.querySelector('.form-div')
-
-
 function renderBrandImages(brand) {
     brand.forEach(brand => {
         const img = document.createElement("img")
         img.className = "brand-image"
-        img.setAttribute('src', )
+        img.setAttribute('src', '')
         img.setAttribute('alt', 'brand-logo')
         img.setAttribute("id", "brand-logo")
         if (brand.id == "1") {
@@ -32,7 +29,6 @@ function renderBrandImages(brand) {
         }
     }) 
 }
-
 function renderAllSneakers(sneaker) {
     sneaker.forEach(pair => {
         if (pair.brand.id == "1") {
@@ -52,47 +48,18 @@ function renderAllSneakers(sneaker) {
             kickContainer.append(kickCard2)
         }
     })
-    
 }
-
-
-function renderAllSneakers(sneaker) {
-    sneaker.forEach(pair => {
-        if (pair.brand.id == "1") {
-            const kickCard1 = document.createElement("div")
-            kickCard1.className = "kick-card-1"
-            kickCard1.innerHTML = `
-            <p>${pair.name}</p>
-            <p>${pair.release}</p>
-            `
-            kickContainer.append(kickCard1)
-        } else if (pair.brand.id == "2") {
-            const kickCard2 = document.createElement("div")
-            kickCard2.innerHTML = `
-            <p>${pair.name}</p>
-            <p>${pair.release}</p>
-            `
-            kickContainer.append(kickCard2)
-        }
-    })
-    
-}
-
-
 function getAllSneakers() {
     fetch("http://localhost:3000/sneakers")
     .then(response => response.json())
     .then(data => 
         renderAllSneakers(data))
 }
-
-
 function getAllBrands() {
     fetch("http://localhost:3000/brands")
     .then(response => response.json())
     .then(data => 
         renderBrandImages(data))
 }
-
 getAllSneakers()
 getAllBrands()
