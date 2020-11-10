7d3facc5b5872e4b0e33fa3494c58663af6f4a81
github username <github email address>
2020-11-10 14:30:07 -0500

    In Progress

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const selectorTwo = document.querySelector(".selector2")
 const selectorThree = document.querySelector(".selector3")
 const selectorFour = document.querySelector(".selector4")
 const selectorFive = document.querySelector(".selector5")
+const kickContainer = document.querySelector(".kick-container")
+const addSneakerForm = document.querySelector('.form-div')
 
 
 function renderBrandImages(brand) {
     brand.forEach(brand => {
         const img = document.createElement("img")
         img.className = "brand-image"
-        img.setAttribute('src', '')
+        img.setAttribute('src', )
         img.setAttribute('alt', 'brand-logo')
         img.setAttribute("id", "brand-logo")
         if (brand.id == "1") {
             img.src = brand.logo
             selectorOne.append(img)
-            console.log(img)
         } else if (brand.id == "2") {
             img.src = brand.logo
             selectorTwo.append(img)
@@ -32,11 +33,37 @@ function renderBrandImages(brand) {
     }) 
 }
 
+function renderAllSneakers(sneaker) {
+    sneaker.forEach(pair => {
+        if (pair.brand.id == "1") {
+            const kickCard1 = document.createElement("div")
+            kickCard1.className = "kick-card-1"
+            kickCard1.innerHTML = `
+            <p>${pair.name}</p>
+            <p>${pair.release}</p>
+            `
+            kickContainer.append(kickCard1)
+        } else if (pair.brand.id == "2") {
+            const kickCard2 = document.createElement("div")
+            kickCard2.innerHTML = `
+            <p>${pair.name}</p>
+            <p>${pair.release}</p>
+            `
+            kickContainer.append(kickCard2)
+        }
+    })
+    
+}
 
 
 
 
-
+function getAllSneakers() {
+    fetch("http://localhost:3000/sneakers")
+    .then(response => response.json())
+    .then(data => 
+        renderAllSneakers(data))
+}
 
 
 function getAllBrands() {
@@ -46,4 +73,5 @@ function getAllBrands() {
         renderBrandImages(data))
 }
 
+getAllSneakers()
 getAllBrands()
